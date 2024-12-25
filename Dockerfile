f0ca42ca14fd9627edd1e86c04dbd23aa5bf7a12
Bram Borggreve <beeman@beeman.nl>
2024-12-25 05:40:24 +0000

    feat: add accounts dir with accounts for alice and bob

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,7 @@ FROM ghcr.io/beeman/solana-test-validator:latest
 
 RUN mkdir -pv /workspace/test-ledger
 
+COPY ./accounts /workspace/accounts
 COPY ./keypairs /workspace/keypairs
 
 COPY ./docker-cmd.sh /workspace/docker-cmd.sh
--- docker-cmd.sh
+++ docker-cmd.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-
+export ACCOUNTS_DIR="./accounts"
 export MPL_CORE="CoREENxT6tW1HoK8ypY1SxRMZTcVPm7R94rH4PZNhX7d"
 export MPL_CORE_DATA="9ZC25KLUrfgSoFVgjE1rrydZBbZns58UXi8A8ZhTdGfr"
 export MPL_METADATA="metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s"
 export MPL_METADATA_DATA="PwDiXFxQsGra4sFFTT8r1QWRMd4vfumiWC1jfWNfdYT"
 
 solana-test-validator \
+    --account-dir $ACCOUNTS_DIR \
     --mint "FeeSoLT7WdoZVXsBPSZc7WKEuhVDVA1TKrNQoHacvxYm" \
     --clone $MPL_CORE --clone $MPL_CORE_DATA \
     --clone $MPL_METADATA --clone $MPL_METADATA_DATA \
